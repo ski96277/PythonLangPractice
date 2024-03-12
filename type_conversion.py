28703d4a65389350df31987e15466efdf88eea8f
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-13 04:00:46 +0600

    simple type convert

---
+++ type_conversion.py
@@ -0,0 +1,23 @@
+numbers = 55
+number_to_string = str(numbers)
+print("number_to_string = ", number_to_string)
+
+numbers = '44'
+string_to_number = int(numbers)
+print("string_to_number = ", string_to_number)
+
+birth_year = input("what is you birth year? ")
+
+year = int(birth_year)  # convert String to Int
+
+age = 2024 - year
+print("my age is = ", age)
+
+# make a simple calculator
+
+first_number = input("First number is = ")
+second_number = input("Second number is = ")
+
+sum = float(first_number) + float(second_number)
+
+print("sum is = ", sum)
