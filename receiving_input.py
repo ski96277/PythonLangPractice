b221ab1df09d7b5ce0203bbaf9c957f6f3c4b140
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-13 03:32:41 +0600

    input taken and print

---
+++ receiving_input.py
@@ -0,0 +1,5 @@
+name = input("WHat is you name? ")
+age = input("How old are you? ")
+salary = input("How much is your salary? ")
+
+print("My Name is", name, "I'm ", age, "years old. My salary is", salary)
