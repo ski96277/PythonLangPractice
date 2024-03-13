0e6e0f77295e759f19a11d8b675bef468d6c054c
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-13 12:43:20 +0600

    comparison operator practice

---
+++ comparison_operator.py
@@ -0,0 +1,42 @@
+x = 5
+y = 5
+
+result = x == y
+print("x any y is same value operator = ", result)
+
+result = x != y
+print("x != y = ", result)
+
+result = x > y
+print("x > y = ", result)
+
+result = x < y
+print("x < y = ", result)
+
+result = x <= y
+print("x <= y = ", result)
+
+result = x >= y
+print("x >= y = ", result)
+
+# Python Comparison Operators
+#  are used to compare two values:
+#
+# Operator	Name	                    Example
+
+# ==	    Equal	                    x == y
+# !=	    Not equal	                x != y
+# >	        Greater than	            x > y
+# <	        Less than	                x < y
+# >=	    Greater than or equal to	x >= y
+# <=	    Less than or equal to	    x <= y
+
+
+# Python Logical Operators
+#  are used to combine conditional statements:
+#
+#  Operator	    Description	                                                    Example
+
+#  and 	    Returns True if both statements are true	                    x < 5 and  x < 10
+#  or	    Returns True if one of the statements is true	                x < 5 or x < 4
+#  not	    Reverse the result, returns False if the result is true	        not(x < 5 and x < 10)
