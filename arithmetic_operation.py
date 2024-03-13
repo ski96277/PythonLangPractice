771cf7e6035e5a0cde7b4cff467f4031d63b94a2
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-13 12:29:56 +0600

    some arrithmetic operation practive

---
+++ arithmetic_operation.py
@@ -0,0 +1,46 @@
+# Addition
+x = 8
+x += 4
+print("Sum of x is = ", x)
+
+# Subtraction
+x = 9
+x -= 4
+print("Subtraction of x is = ", x)
+
+# Multiplication
+x = 9
+x *= 4
+print("Multiplication of x is = ", x)
+
+# Division
+x = 9
+x /= 4
+print("Division of x is = ", x)
+
+# Modulus
+x = 9
+x %= 4
+print("Modulus of x is = ", x)
+
+# Exponentiation
+x = 9
+x **= 2
+print("Exponentiation of x is = ", x)
+
+# Floor division
+x = 9
+x //= 2
+print("Floor division of x is = ", x)
+
+# Python Arithmetic Operators
+#  are used with numeric values to perform common mathematical operations:
+#
+#   Operator	    Name	        Example
+#       +	      Addition	         x + y
+#       -	      Subtraction	     x - y
+#       *	      Multiplication	 x * y
+#       /	      Division	         x / y
+#       %	      Modulus	         x % y
+#       **        Exponentiation	 x ** y
+#       //	      Floor division	 x // y
