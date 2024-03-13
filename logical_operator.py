087df436e257ca506608ab20aec77462067eca95
Shaikh Imran Hossain <imran@Shaikhs-MacBook-Pro.local>
2024-03-13 12:52:14 +0600

    logical operator added

---
+++ logical_operator.py
@@ -0,0 +1,12 @@
+price = 25
+
+print(price > 10 or price < 30)
+print(price > 10 and price < 30)
+print(not price > 10)
+
+# Python has 3 logical operator
+# and (both need to be true)
+# or (at least one need to be true)
+# not (inverses any value that we give)
+
+
